892ccb1a64b724d24fb12702a90b2011601c0ee9
spartan4604 <82423478+spartan4604@users.noreply.github.com>
2021-04-17 16:08:43 -0400

    Add files via upload

---
+++ NewUser.ps1
@@ -0,0 +1,40 @@
+﻿# Creating new AD User
+# This script assumes you have already installed the MSOnline and ActiveDirectory modules
+
+$date = Get-Date -Format "MM-dd-yyyy-HH-mm"
+
+Start-Transcript -Path "C:\New-Account-$date.log" -Append
+
+Import-Module ActiveDirectory
+
+Import-Module MSOnline
+
+Write-Warning "THIS SCRIPT IS FOR CREATION OF A NEW USER. PLEASE ENSURE ACCURACY IN EXECUTION AND DATA ENTRY. BY DEFAULT THIS ACCOUNT WILL BE STORED IN THE 'USERS' OU AND WILL NEED TO BE MOVED."
+
+$usercheck = Read-Host -Prompt "Enter the intended username of the user being created"
+
+$name = "$usercheck"
+$User = $(try {Get-ADUser $Name} catch {$null})
+If ($User -ne $Null) { 
+"USERNAME ALREADY EXISTS. DO NOT CONTINUE."
+} Else
+ {
+"User not found in AD. Proceed with creation."}
+
+$name = Read-Host -Prompt "Enter the first and last name of the user being created"
+
+$firstname = Read-Host -Prompt "Enter the first name of the user being created"
+
+$lastname = Read-Host -Prompt "Enter the last name of the user being created"
+
+$description = Read-Host -Prompt "Enter the description/job title of the user being created"
+
+$email = "$firstname.$lastname@simradiology.onmicrosoft.com"
+
+$employeeid = Read-Host -Prompt "Enter the employee ID of the user"
+
+$manager = Read-Host -Prompt "Enter the SAM of the user's manager"
+
+$office = Read-Host -Prompt "Enter the user's work location"
+
+New-ADUser -Name "$name" -DisplayName "$name" -GivenName "$firstname" -Surname "$lastname" -SamAccountName "$usercheck" -UserPrincipalName "$email" -EmailAddress "$email" -Description "$description" -Title "$description" -Manager "$manager" -Office "$office" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true -ChangePasswordAtLogon $true
---
+++ Re-EnableUser.ps1
@@ -0,0 +1,45 @@
+﻿# Description: Quick Re-Enable of existing AD User
+# Date Started: 04/14/2021
+# This script assumes you have already installed and connected to MSOnline and ActiveDirectory Modules
+
+Import-Module ActiveDirectory
+
+Import-Module MSOnline
+
+$date = Get-Date -Format "MM-dd-yyyy-HH-mm"
+
+$user = Read-Host -Prompt "Enter the username of the user being re-enabled"
+
+Start-Transcript -Path "C:\Users\jeff.johnson\OneDrive - US Radiology Specialists Inc\Windows PS Logs\ReEnable-Account-$user-$date.log" -Append
+
+Get-ADUser -Identity $user -Properties *
+
+$mirroruser = Read-Host -Prompt "Enter the username of the user to mirror"
+
+$E1 = "reseller-account:STANDARDPACK"
+
+$E3 = "reseller-account:ENTERPRISEPACK"
+
+Enable-ADAccount -Identity $user -Confirm
+
+Unlock-ADAccount -Identity $user -Confirm
+
+Set-ADAccountPassword -Identity $user -Reset -Confirm
+
+Set-ADUser -Identity $user -Description (Read-Host -Prompt "Enter the description of the user being re-enabled") -Confirm
+
+Get-ADUser -Identity $mirroruser -Properties * | Select-Object Memberof -ExpandProperty Memberof | Add-ADGroupMember -Members $user -Confirm
+
+Set-ADUser -Identity $user -ChangePasswordAtLogon $true -Confirm
+
+Set-MsolUser -UserPrincipalName "$user@usradiology.com" -UsageLocation US
+
+Set-MsolUser -UserPrincipalName "$user@usradiology.com" -BlockCredential $false
+
+Get-MsolAccountSku | fl AccountSkuId,ActiveUnits,ConsumedUnits
+
+Set-MsolUserLicense -UserPrincipalName "$user@usradiology.com" -AddLicenses (Read-Host -Prompt "Enter 365 license to add by variable name (E1, E3)")
+
+Get-MsolUser -UserPrincipalName "$user@usradiology.com"
+
+Stop-Transcript
