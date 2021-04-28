7a1d25cd0edf0cb0a6813683b06be901d6d3912a
spartan4604 <82423478+spartan4604@users.noreply.github.com>
2021-04-28 07:29:19 -0400

    Add files via upload

---
+++ QuickStart (Public).ps1
@@ -0,0 +1,13 @@
+﻿# QuickStart
+
+$date = Get-Date -Format "MMddyyyy-HHmm"
+
+Import-Module ActiveDirectory
+
+Connect-MsolService
+
+Connect-ExchangeOnline
+
+$start = "C:\Log-$date.log"
+
+$term = "‪C:\TerminatingUser (Current).ps1"
